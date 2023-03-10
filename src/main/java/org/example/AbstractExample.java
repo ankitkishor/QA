d1714019939f45ab74b58f17f05e402aaa0b9703
ankit <ankit.kishorelv@gmail.com>
2023-03-10 14:53:28 +0530

    Initial commit

---
+++ src/main/java/org/example/AbstractExample.java
@@ -0,0 +1,9 @@
+/*
+* Author : Ankit Kishor
+* Date: 02-03-2023
+* Created with :Intellij IDEA Community Edition
+*/
+
+
+package org.example;public class AbstractExample {
+}
---
+++ src/main/java/org/example/Palindrome.java
@@ -0,0 +1,9 @@
+/*
+* Author : Ankit Kishor
+* Date: 01-03-2023
+* Created with :Intellij IDEA Community Edition
+*/
+
+
+package org.example;public class Palindrome {
+}
